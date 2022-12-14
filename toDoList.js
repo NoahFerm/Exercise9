9aaa187f0eeb4aa92724ceec9ff5afa50e868235
Zxewl <noahferm@gmail.com>
2022-12-14 10:03:08 +0100

    Small fixes

--- toDoList.js
+++ toDoList.js
@@ -1,13 +1,13 @@
 //Doesn't work 
 
-// const button = document.getElementById('addToList');
-// button.addEventListener('click', addItem())
+const button = document.getElementById('addToList');
+button.addEventListener('click', addItem)
 
-// function addItem(){
-//     let list = document.getElementById("list");
-//     let input = document.getElementById("listInput").value;
-//     let li = document.createElement("li");
-//     li.setAttribute('id', input);
-//     li.appendChild(document.createTextNode(input));
-//     list.appendChild(li);
-// }
+function addItem(){
+    let list = document.getElementById("list");
+    let input = document.getElementById("listInput").value;
+    let li = document.createElement("li");
+    li.setAttribute('id', input);
+    // li.appendChild(document.createTextNode(input));
+    list.appendChild(input);
+}
